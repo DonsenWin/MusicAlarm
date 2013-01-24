00c1bf92a3bb80c276288ab7d0f36a63d275dd93
takahiro.ono <oono0114@gmail.com>
2013-01-24 22:44:19 +0900

    modify for unittest

--- MusicAlarm/src/jp/fedom/android/musicalarm/MusicAlarm.java
+++ MusicAlarm/src/jp/fedom/android/musicalarm/MusicAlarm.java
@@ -1,28 +1,25 @@
-//package jp.fedom.android.musicalarm;
-//
-//import android.app.Application;
-//
-///**
-// * @author taka2
-// * TODO:describe comment 
-// */
-//public final class MusicAlarm extends Application {
-//	
-//	private int dummy;
-//     
-//    /**
-//     * This is dummy constructor
-//     * TODO:describe comment 
-//     */
-//     public MusicAlarm() {
-//         super();
-//         dummy = 1;
-//     }
-//
-//     /**
-//      * This is dummy constructor
-//      * TODO:describe comment 
-//      */
-//
-//     
-//}
+package jp.fedom.android.musicalarm;
+
+import android.app.Application;
+
+/**
+ * @author taka2
+ * TODO:describe comment 
+ */
+public final class MusicAlarm extends Application {
+	
+    /**
+     * This is dummy constructor
+     * TODO:describe comment 
+     */
+     public MusicAlarm() {
+         super();
+     }
+
+     /**
+      * This is dummy constructor
+      * TODO:describe comment 
+      */
+
+     
+}
--- MusicAlarmTest/src/jp/fedom/android/musicalarm/test/activity/MainActivityTest.java
+++ MusicAlarmTest/src/jp/fedom/android/musicalarm/test/activity/MainActivityTest.java
@@ -1,7 +1,7 @@
 // CHECKSTYLE:OFF
-/*package jp.fedom.android.musicalarm.test.activity;
+package jp.fedom.android.musicalarm.test.activity;
 
-import jp.fedom.android.musicalarm.MainActivity;
+import jp.fedom.android.musicalarm.activity.MainActivity;
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
@@ -12,36 +12,36 @@ public class MainActivityTest extends
 	private Activity targetActivity; 
 	private Instrumentation instrumention; 
 	
-	*//**
+	/**
 	 * 
-	 *//*
+	 */
 	public MainActivityTest() {
-		super(jp.fedom.android.musicalarm.MainActivity.class);
+		super(jp.fedom.android.musicalarm.activity.MainActivity.class);
 	}
 
-	*//**
+	/**
 	 * 
-	 *//*
+	 */
 	protected void setUp() throws Exception {
 		super.setUp();
 		targetActivity = getActivity();
 		instrumention  = getInstrumentation();
 	}
 
-	*//**
+	/**
 	 * 
-	 *//*
+	 */
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
 
-	*//**
+	/**
 	 * 
-	 *//*
+	 */
 	public void test_sample() {
 		
 	}
 
 }
-*/
+
 // CHECKSTYLE:ON
