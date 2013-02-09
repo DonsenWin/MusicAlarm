03d60ee91bb1511d7c933c92ac4cc80db1f38b84
takahiro.ono <oono0114@gmail.com>
2013-02-09 10:34:01 +0900

    error handle for file not found

--- MusicAlarm/src/jp/fedom/android/musicalarm/util/music/MusicWapper.java
+++ MusicAlarm/src/jp/fedom/android/musicalarm/util/music/MusicWapper.java
@@ -1,5 +1,6 @@
 package jp.fedom.android.musicalarm.util.music;
 
+import java.io.File;
 import java.io.IOException;
 
 import android.media.AudioManager;
@@ -13,78 +14,79 @@ import android.util.Log;
  * 
  */
 public enum MusicWapper {
-    /** Singleton implement. */
-    instance;
+	/** Singleton implement. */
+	instance;
 
-    /** tag for log. */
-    private static final String TAG = "MusicWapper";
+	/** tag for log. */
+	private static final String TAG = "MusicWapper";
 
-    /** Music Player. */
-    private MediaPlayer musicPlayer;
+	/** Music Player. */
+	private MediaPlayer musicPlayer;
 
-    /** Audio Player. */
-    private AudioManager audioManager;
+	/** Audio Player. */
+	private AudioManager audioManager;
 
-    /**
-     * for Singleton.
-     * 
-     * @return singleInstance of this class
-     */
-    public static MusicWapper getInstance() {
-        return instance;
-    }
+	/**
+	 * for Singleton.
+	 * 
+	 * @return singleInstance of this class
+	 */
+	public static MusicWapper getInstance() {
+		return instance;
+	}
 
-    /**
-     * getSystemService(Context.AUDIO_SERVICE).
-     * 
-     * @param manager
-     *            please (AudioManager) getSystemService(Context.AUDIO_SERVICE)
-     * @param path
-     *            path to audio file
-     */
-    public void start(final AudioManager manager, final String path) {
-        if (manager == null) {
-            Log.w(TAG, "called start with null");
-            return;
-        }
-        if (musicPlayer != null) {
-            stop();
-        }
+	/**
+	 * getSystemService(Context.AUDIO_SERVICE).
+	 * 
+	 * @param manager
+	 *            please (AudioManager) getSystemService(Context.AUDIO_SERVICE)
+	 * @param path
+	 *            path to audio file
+	 */
+	public void start(final AudioManager manager, final String path) {
+		if (manager == null) {
+			Log.w(TAG, "called start with null");
+			return;
+		}
+		if (musicPlayer != null) {
+			stop();
+		}
 
-        this.audioManager = manager;
-        this.audioManager.setBluetoothScoOn(true);
-        this.audioManager.setMode(AudioManager.MODE_IN_CALL);
+		this.audioManager = manager;
+		this.audioManager.setBluetoothScoOn(true);
+		this.audioManager.setMode(AudioManager.MODE_IN_CALL);
 
-        musicPlayer = new MediaPlayer();
-        try {
-            musicPlayer.setDataSource(path);
-            musicPlayer.setAudioStreamType(AudioManager.STREAM_VOICE_CALL);
-            musicPlayer.prepare();
-        } catch (IllegalArgumentException e) {
-            Log.e(TAG, "IllegalArgumentException", e);
-        } catch (SecurityException e) {
-            Log.e(TAG, "SecurityException", e);
-        } catch (IllegalStateException e) {
-            Log.e(TAG, "IllegalStateException", e);
-        } catch (IOException e) {
-            Log.e(TAG, "IOException", e);
-        }
+		if ((new File(path)).exists()) {
+			musicPlayer = new MediaPlayer();
+			try {
+				musicPlayer.setDataSource(path);
+				musicPlayer.setAudioStreamType(AudioManager.STREAM_VOICE_CALL);
+				musicPlayer.prepare();
+			} catch (IllegalArgumentException e) {
+				Log.e(TAG, "IllegalArgumentException", e);
+			} catch (SecurityException e) {
+				Log.e(TAG, "SecurityException", e);
+			} catch (IllegalStateException e) {
+				Log.e(TAG, "IllegalStateException", e);
+			} catch (IOException e) {
+				Log.e(TAG, "IOException", e);
+			}
+			musicPlayer.start();
+		}
+	}
 
-        musicPlayer.start();
-    }
+	/**
+	 * This is dummy comment. TODO: update comment
+	 */
+	public void stop() {
+		if (musicPlayer != null) {
+			musicPlayer.stop();
+		}
 
-    /**
-     * This is dummy comment. TODO: update comment
-     */
-    public void stop() {
-        if (musicPlayer != null) {
-            musicPlayer.stop();
-        }
-
-        if (audioManager != null) {
-            audioManager.setMode(AudioManager.MODE_NORMAL);
-            audioManager.setBluetoothScoOn(false);
-        }
-    }
+		if (audioManager != null) {
+			audioManager.setMode(AudioManager.MODE_NORMAL);
+			audioManager.setBluetoothScoOn(false);
+		}
+	}
 
 }
