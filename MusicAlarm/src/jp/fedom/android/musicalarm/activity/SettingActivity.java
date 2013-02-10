146c4724ed46d0a09e584ec9b23945cb87a4e2e9
takahiro.ono <oono0114@gmail.com>
2013-02-10 16:17:10 +0900

    develop config screen

--- MusicAlarm/src/jp/fedom/android/musicalarm/activity/SettingActivity.java
+++ MusicAlarm/src/jp/fedom/android/musicalarm/activity/SettingActivity.java
@@ -1,11 +1,24 @@
 package jp.fedom.android.musicalarm.activity;
 
+import java.util.ArrayList;
+
 import jp.fedom.android.musicalarm.R;
+import jp.fedom.android.musicalarm.item.ConfigItem;
+import jp.fedom.android.musicalarm.item.ConfigPreference;
+import jp.fedom.android.musicalarm.item.ConfigItem.DateType;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -13,32 +26,184 @@ import android.view.Menu;
  */
 public final class SettingActivity extends PreferenceActivity {
 
-    /** The Constant TAG. */
-    private static final String TAG = "SettingActivity";
+	/** The Constant TAG. */
+	private static final String TAG = "SettingActivity";
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see android.preference.PreferenceActivity#onCreate(android.os.Bundle)
-     */
-    @Override
-    protected void onCreate(final Bundle savedState) {
-        super.onCreate(savedState);
-        addPreferencesFromResource(R.layout.activity_setting);
-        final Intent intent = getIntent();
-        final int defaultInt = -1;
-        Log.d(TAG , "config index= " + intent.getIntExtra("configIndex", defaultInt));
-    }
+	/** The Constant INVALIDATE. */
+	private static final int INVALIDATE = -1;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
-     */
-    @Override
-    public boolean onCreateOptionsMenu(final Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_setting, menu);
-        return true;
-    }
+	/** The config index. */
+	private int configIndex = INVALIDATE;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.preference.PreferenceActivity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(final Bundle savedState) {
+		super.onCreate(savedState);
+		addPreferencesFromResource(R.layout.activity_setting);
+		final Intent intent = getIntent();
+		configIndex = intent.getIntExtra("configIndex", INVALIDATE);
+		Log.d(TAG, "config index= " + configIndex);
+		if (configIndex == INVALIDATE) {
+			this.finish();
+			Log.d(TAG, "stop with some happen. config index is INVALIDATE");
+		}
+		ConfigPreference pref = new ConfigPreference(
+				PreferenceManager.getDefaultSharedPreferences(this));
+
+		ConfigItem item = pref.loadConfigItems().get(configIndex);
+
+		// TODO: implement correct
+		/* set default data from config item */
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+//		PreferenceManager manager = this.getPreferenceManager();
+		Editor defaultEditor = prefs.edit();
+		defaultEditor
+				.putBoolean(getString(R.string.preference_item_enable_key),
+						item.isEnable());
+		defaultEditor.putString(getString(R.string.preference_item_title_key),
+				item.getTitle());
+		defaultEditor.putString(
+				getString(R.string.preference_list_datetype_key), item
+						.getDateType().name());
+		defaultEditor.apply();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(final Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onKeyDown(int, android.view.KeyEvent)
+	 */
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_BACK:
+			if (isChangedConfig()) {
+				showSaveDiscardAlart();
+			} else {
+				finish();
+			}
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onUserLeaveHint()
+	 */
+	@Override
+	public void onUserLeaveHint() {
+		if (isChangedConfig()) {
+			showSaveDiscardAlart();
+		}
+	}
+
+	/**
+	 * This is dummy comment. TODO:describe comment
+	 */
+	private void saveConfig() {
+		Log.d("menu", "called save");
+		ConfigPreference pref = new ConfigPreference(
+				PreferenceManager.getDefaultSharedPreferences(this));
+		ArrayList<ConfigItem> list = (ArrayList<ConfigItem>) pref
+				.loadConfigItems();
+		list.set(configIndex, getConfigfromUI());
+		pref.saveConfigItems(list);
+	}
+
+	/**
+	 * Gets the configfrom ui.
+	 * 
+	 * @return the configfrom ui
+	 */
+	private ConfigItem getConfigfromUI() {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		boolean enable = prefs.getBoolean(
+				getString(R.string.preference_item_enable_key), false);
+		String title = prefs.getString(
+				getString(R.string.preference_item_title_key), "title "
+						+ configIndex);
+		String datatype = prefs.getString(
+				getString(R.string.preference_list_datetype_key),
+				DateType.daily.name());
+
+		return new ConfigItem(enable, title, "00:00", "path",
+				DateType.convert(datatype));
+	}
+
+	/**
+	 * Show save discard alart.
+	 */
+	private void showSaveDiscardAlart() {
+		new AlertDialog.Builder(this)
+				.setTitle("Save/Discard")
+				.setMessage("Save Config Change?")
+				.setPositiveButton("Save",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								(Toast.makeText(SettingActivity.this, "saved",
+										Toast.LENGTH_SHORT)).show();
+								saveConfig();
+								finish();
+							}
+						})
+				.setNegativeButton("Discard",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								(Toast.makeText(SettingActivity.this,
+										"discarded", Toast.LENGTH_SHORT))
+										.show();
+								finish();
+							}
+						}).show();
+	}
+
+	/**
+	 * Checks if is changed config.
+	 * 
+	 * @return true, if is changed config
+	 */
+	private boolean isChangedConfig() {
+		final ConfigPreference pref = new ConfigPreference(
+				PreferenceManager.getDefaultSharedPreferences(this));
+		ConfigItem previous = pref.loadConfigItems().get(configIndex);
+		return !(previous.equals(getConfigfromUI()));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+	 */
+	public boolean onOptionsItemSelected(final MenuItem item) {
+		boolean returnValue = false;
+		if (item.getItemId() == R.id.menu_save) {
+			saveConfig();
+			returnValue = true;
+		} else {
+			returnValue = super.onOptionsItemSelected(item);
+		}
+		return returnValue;
+	}
 
 }
--- MusicAlarm/src/jp/fedom/android/musicalarm/item/ConfigPreference.java
+++ MusicAlarm/src/jp/fedom/android/musicalarm/item/ConfigPreference.java
@@ -9,85 +9,100 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.util.Log;
 
+// TODO: Auto-generated Javadoc
 /**
  * dummy comment. TODO:update comment
  * 
  * @author taka
  */
 public class ConfigPreference {
-    /** dummy comment. TODO:update comment */
-    private final SharedPreferences preference;
+	/** dummy comment. TODO:update comment */
+	private final SharedPreferences preference;
 
-    /** dummy comment. TODO:update comment */
-    private static final String PREFERENCE_KEY = "musicalarm_config";
+	/** dummy comment. TODO:update comment */
+	private static final String PREFERENCE_KEY = "musicalarm_config";
 
-    /** dummy comment. TODO:update comment */
-    private static final int ITEM_NUM = 4;
+	/** dummy comment. TODO:update comment */
+	private static final int ITEM_NUM = 4;
 
-    /** dummy comment. TODO:update comment */
-    private static final int ZERO = 0;
+	/** dummy comment. TODO:update comment */
+	private static final int ZERO = 0;
 
-    /** dummy comment. TODO:update comment */
-    private static final String TAG = "ConfigPreference";
+	/** dummy comment. TODO:update comment */
+	private static final String TAG = "ConfigPreference";
 
-    /**
-     * dummy comment. TODO:update comment
-     * 
-     * @param argPreference
-     *            preference
-     */
-    public ConfigPreference(final SharedPreferences argPreference) {
-        this.preference = argPreference;
-    }
+	/**
+	 * dummy comment. TODO:update comment
+	 * 
+	 * @param argPreference
+	 *            preference
+	 */
+	public ConfigPreference(final SharedPreferences argPreference) {
+		this.preference = argPreference;
+	}
 
-    /**
-     * dummy comment. TODO:update comment
-     * 
-     * @return list if under ITEM_NUM, padding inialized item.
-     */
-    public final List<ConfigItem> loadConfigItems() {
-        ArrayList<ConfigItem> items;
-        final String jsonData = preference.getString(PREFERENCE_KEY, "");
+	/**
+	 * Reset all config items.
+	 */
+	public final void resetAllConfigItems() {
+		ArrayList<ConfigItem> items = new ArrayList<ConfigItem>();
+		for (int i = 0; i < ITEM_NUM; i++) {
+			items.add(new ConfigItem());
+		}
+		saveConfigItems(items);
 
-        if (jsonData == null || jsonData.isEmpty()) {
-            items = new ArrayList<ConfigItem>();
-        } else {
-            try {
-                items = (ArrayList<ConfigItem>) ConfigItem.JsonParser.getInstance().parse(jsonData);
-            } catch (JSONException e) {
-                items = new ArrayList<ConfigItem>();
-            }
-        }
-        final int itemCountDelta = ITEM_NUM - items.size();
-        if (itemCountDelta > ZERO) {
-            for (int i = 0; i < itemCountDelta; i++) {
-                items.add(new ConfigItem());
-            }
-        } else if (itemCountDelta < ZERO) {
-            for (int i = itemCountDelta; i < 0; i++) {
-                items.remove(ITEM_NUM + itemCountDelta);
-            }
-        }
-        return items;
-    }
+	}
 
-    /**
-     * dummy comment. TODO:update comment
-     * 
-     * @param items
-     *            items
-     */
-    public final void saveConfigItems(final List<ConfigItem> items) {
-        if (items != null) {
-            final Editor editor = preference.edit();
-            try {
-                editor.putString(PREFERENCE_KEY, ConfigItem.JsonGenerator.getInstance().genJson(items));
-                editor.apply();
-            } catch (JSONException e) {
-                // do nothing
-                Log.w(TAG, "SAVE_PREFERMCE_ERROR");
-            }
-        }
-    }
+	/**
+	 * dummy comment. TODO:update comment
+	 * 
+	 * @return list if under ITEM_NUM, padding inialized item.
+	 */
+	public final List<ConfigItem> loadConfigItems() {
+		ArrayList<ConfigItem> items;
+		final String jsonData = preference.getString(PREFERENCE_KEY, "");
+
+		if (jsonData == null || jsonData.isEmpty()) {
+			items = new ArrayList<ConfigItem>();
+		} else {
+			try {
+				items = (ArrayList<ConfigItem>) ConfigItem.JsonParser
+						.getInstance().parse(jsonData);
+			} catch (JSONException e) {
+				items = new ArrayList<ConfigItem>();
+			}
+		}
+		final int itemCountDelta = ITEM_NUM - items.size();
+		if (itemCountDelta > ZERO) {
+			for (int i = 0; i < itemCountDelta; i++) {
+				items.add(new ConfigItem());
+			}
+		} else if (itemCountDelta < ZERO) {
+			for (int i = itemCountDelta; i < 0; i++) {
+				items.remove(ITEM_NUM + itemCountDelta);
+			}
+		}
+		return items;
+	}
+
+	/**
+	 * dummy comment. TODO:update comment
+	 * 
+	 * @param items
+	 *            items
+	 */
+	public final void saveConfigItems(final List<ConfigItem> items) {
+		if (items != null) {
+			final Editor editor = preference.edit();
+			try {
+				editor.putString(PREFERENCE_KEY, ConfigItem.JsonGenerator
+						.getInstance().genJson(items));
+				editor.clear().apply();
+			} catch (JSONException e) {
+				// do nothing
+				Log.w(TAG, "SAVE_PREFERMCE_ERROR");
+			}
+		}
+	}
 
 }
